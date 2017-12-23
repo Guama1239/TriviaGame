7684a768b5bbb198bc48a2897c58521a0c05e9a0
Guama1239 <guama1239@gmail.com>
2017-12-23 15:52:28 -0600

    changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// SETUP VARIABLES
+//======================
+// FUNCTIONS
+//======================
+// MAIN PROCESSESS
+//======================
+
+//1. Retrieve user inputs and convert to variables
+//2. Use those variables to compare with right answers
