b508898cfbbffa44cc17a6a9cc062f9ed75a57d8
Guama1239 <guama1239@gmail.com>
2017-12-23 23:01:00 -0600

    improvements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,12 @@
 
 //1. Retrieve user inputs and convert to variables
 //2. Use those variables to compare with right answers
+$(document).ready(function(){
+    $("#questions").ready(function(){$("#questions").hide();})
+});
+
+    $("#start").click(function(){
+    $("#questions").show();
+    $("#start").hide();
+});
+//$(document).ready(function(){})
