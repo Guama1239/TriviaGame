539bde9681b630d3eb2f899e50bfa2730fb463ef
Guama1239 <guama1239@gmail.com>
2017-12-24 01:14:39 -0600

    more functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
 // SETUP VARIABLES
+var doneButton = $("#done");
+var countDown = 5;
+var intervalId;
 //======================
 // FUNCTIONS
 //======================
@@ -7,12 +10,31 @@
 
 //1. Retrieve user inputs and convert to variables
 //2. Use those variables to compare with right answers
+//
+//this hides temporarly the trivia questions until the start button is clicked
 $(document).ready(function(){
     $("#questions").ready(function(){$("#questions").hide();})
 });
-
+//once the start button is clicked, it will hide the start
+//button and display the trivia questions and activates the countdown.
     $("#start").click(function(){
     $("#questions").show();
+    intervalId = setInterval(decrement, 1000);
     $("#start").hide();
 });
+
+function decrement(){
+    // decrease countDown by one second
+    countDown--;
+    $("#timeremaining").html("<h2>" + countDown + "</h2>");
+    // once countDown hits zero
+    if (countDown === 0 ){
+        stop();
+    }
+}
+
+function stop(){
+    clearInterval(intervalId);
+    $("#questions").hide();
+}
 //$(document).ready(function(){})
